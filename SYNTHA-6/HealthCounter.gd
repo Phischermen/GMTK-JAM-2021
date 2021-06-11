8c2300e4c33fd7d417d8aa8184f305685c859956
Kevin Fischer <46539626+Phischermen@users.noreply.github.com>
2021-06-11 13:49:02 -0700

    Cleaned up some names

--- SYNTHA-6/HealthCounter.gd
+++ SYNTHA-6/HealthCounter.gd
@@ -1,27 +1,23 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var amount = 9
-	_setHealth(amount)
+	var amount = 5
+	set_health(amount)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func _setHealth(amount):
-	var healthCount = 9;
+
+
+func set_health(amount):
 	
 	# Set the color of the hearts.
 	for heart in $HBoxContainer.get_children():
 		heart.modulate = Color.crimson
 		amount -= 1
-		
 		if (amount >= 0):
 			# Set the filled in hearts.			
 			heart.modulate = Color.crimson
