68e414d51970fc4024d144218fc2aa7347c46621
gdunford99 <56702202+gdunford99@users.noreply.github.com>
2021-06-11 21:32:27 -0700

    Initial Dumb Enemy Push
    Dumb enemy that follows the player when they enter a certain radius. Does not navigate or path at all

---
+++ SYNTHA-6/Dumb_Enemy.gd
@@ -0,0 +1,47 @@
+extends "res://Enemy.gd"
+
+class_name Dumb
+
+
+export var dumbEnemySpeed = 200
+var velocity = Vector2.ZERO
+var player = null
+
+export var health = 100
+export var shootingDistance = 500
+
+enum State {MOVING, SHOOTING}
+
+var state = State.MOVING
+
+var dogs = []
+
+var distance = 100000
+
+
+func _physics_process(delta):
+	velocity = Vector2.ZERO
+	
+	if player != null:
+		velocity = position.direction_to(player.position) * dumbEnemySpeed
+	else:
+		velocity = Vector2.ZERO
+		
+	velocity = velocity.normalized()
+	velocity = move_and_collide(velocity)
+
+func take_damage(amount:int):
+	health -= amount
+	
+
+#checks for player if they are in Area2D and are in group Dog
+func _on_Area2D_body_entered(body):
+	if body != self and body.is_in_group("Dog"):
+		player = body
+
+#no longer follows player if they leave Area2D
+func _on_Area2D_body_exited(body):
+	player = null
+
+func _on_Timer_timeout():
+	pass
