0d03c4c329905dd4056d506fc09ad3995e3fc7f5
chrispychip <61853339+chrispychip@users.noreply.github.com>
2021-06-11 22:23:50 -0700

    Menu button direct update
    The menu buttons lead to existing pages now and the level selection leads to an existing page of levels that will also redirect.

---
+++ SYNTHA-6/Scripts/Main Menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_StartButton_pressed():
+	get_tree().change_scene("res://Levels/Gameplay_Level 1.tscn") 
+
+
+func _on_LevelButton_pressed():
+	get_tree().change_scene("res://Levels/LevelsMenu.tscn")
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+
+
+func _on_CreditButton_pressed():
+	get_tree().change_scene("res://Levels/Credits.tscn")
